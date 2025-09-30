39179abd42abbe46abca0f21ed115d0edc0bdbfd
Pramod Kumar <pkumar030292@gmail.com>
2025-09-30 20:45:46 +0530

    Removed unnecessary files

--- REST_TEST/utils/config.py
+++
@@ -1,9 +0,0 @@
-# utils/config.py
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent   # project root
-TEMPLATE_DIR = BASE_DIR / "templates"
-OUTPUT_DIR = BASE_DIR / "outputs"
-
-TEMPLATE_DIR.mkdir(parents=True, exist_ok=True)
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
